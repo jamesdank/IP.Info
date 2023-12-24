f038e4256ed0b33a34c3a20c87bd4f32d99718b0
jamesdank <94645949+jamesdank@users.noreply.github.com>
2023-12-24 10:47:11 -0600

    Update ip.info.php

--- ip.info.php
+++ ip.info.php
@@ -1,7 +1,7 @@
 <?php
 
 $yellow = "\033[01;33m";
-$White_green = "\033[01;37m>\033[01;32m";
+$white_green = "\033[01;37m>\033[01;32m";
 $reset = "\033[0m";
 
 $file = "ip.info.txt";
@@ -20,21 +20,21 @@ if (!file_exists($file)) {
 
 if(!empty($data['status']) && $data['status'] == 'success') {
     
-    echo " " . $yellow . "External IP   " . $White_green . "   " . $data['query'];
-    echo "\n " . $yellow . "Country code  " . $White_green . "   " . $data['countryCode'];
-    echo "\n " . $yellow . "Country       " . $White_green . "   " . $data['country'];
-    echo "\n " . $yellow . "Date & Time   " . $White_green . "   " . date("F j, Y, g:i a");
-    echo "\n " . $yellow . "Region code   " . $White_green . "   " . $data['region'];
-    echo "\n " . $yellow . "Region        " . $White_green . "   " . $data['regionName'];
-    echo "\n " . $yellow . "City          " . $White_green . "   " . $data['city'];
-    echo "\n " . $yellow . "Zip code      " . $White_green . "   " . $data['zip'];
-    echo "\n " . $yellow . "Time zone     " . $White_green . "   " . $data['timezone'];
-    echo "\n " . $yellow . "ISP           " . $White_green . "   " . $data['isp'];
-    echo "\n " . $yellow . "Organization  " . $White_green . "   " . $data['org'];
-    echo "\n " . $yellow . "ASN           " . $White_green . "   " . $data['as'];
-    echo "\n " . $yellow . "Latitude      " . $White_green . "   " . $data['lat'];
-    echo "\n " . $yellow . "Longtitude    " . $White_green . "   " . $data['lon'];
-    echo "\n " . $yellow . "Location      " . $White_green . "   " . $data['lat'] . "," . $data['lon'];
+    echo " " . $yellow . "External IP   " . $white_green . "   " . $data['query'];
+    echo "\n " . $yellow . "Country code  " . $white_green . "   " . $data['countryCode'];
+    echo "\n " . $yellow . "Country       " . $white_green . "   " . $data['country'];
+    echo "\n " . $yellow . "Date & Time   " . $white_green . "   " . date("F j, Y, g:i a");
+    echo "\n " . $yellow . "Region code   " . $white_green . "   " . $data['region'];
+    echo "\n " . $yellow . "Region        " . $white_green . "   " . $data['regionName'];
+    echo "\n " . $yellow . "City          " . $white_green . "   " . $data['city'];
+    echo "\n " . $yellow . "Zip code      " . $white_green . "   " . $data['zip'];
+    echo "\n " . $yellow . "Time zone     " . $white_green . "   " . $data['timezone'];
+    echo "\n " . $yellow . "ISP           " . $white_green . "   " . $data['isp'];
+    echo "\n " . $yellow . "Organization  " . $white_green . "   " . $data['org'];
+    echo "\n " . $yellow . "ASN           " . $white_green . "   " . $data['as'];
+    echo "\n " . $yellow . "Latitude      " . $white_green . "   " . $data['lat'];
+    echo "\n " . $yellow . "Longtitude    " . $white_green . "   " . $data['lon'];
+    echo "\n " . $yellow . "Location      " . $white_green . "   " . $data['lat'] . "," . $data['lon'];
     echo "\n\n$reset";
 }
 
